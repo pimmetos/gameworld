a3bd47e0454f9f787ddd0c2883cc5fc154bc3c7f
Pim <pimmetos@hotmail.com>
2018-11-12 14:27:03 +0100

    Initial commit

---
+++ index.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="utf-8"/>
+    <meta name="description" content="Elite games. - Enjoy The Greatest Hits">
+    <link rel="stylesheet" type="text/css" href="css/style.css">
+    <title>Radio Gaga</title>
+</head>
+<body>
+<?php
+include "header.html";
+?>
+
+<img class="homePagePicture" src="pictures/eliteGames.jpg">
+<h1>Welcome to elite games</h1>
+<h2>For all the elite buyers</h2>
+<?php
+include "footer.html"
+?>
+</body>
+</html>
